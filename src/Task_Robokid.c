e67f73d3b979cf3de80c485ea378892f0508cf4d
Jim Herd <jtherd@gmail.com>
2022-02-28 14:04:03 +0000

    Motor command packet simplified

--- src/Task_Robokid.c
+++ src/Task_Robokid.c
@@ -1,3 +1,9 @@
+/**
+ * @file   Task_Robokid.c
+ * @author Jim Herd
+ * @brief  Run user level Robokid system
+ * @date   2022-02-28
+ */
 #include "system.h"
 
 #include "pico/stdlib.h"
@@ -16,21 +22,21 @@ void Task_Robokid(void *p) {
 motor_cmd_packet_t  motor_cmd_packet;
 
     FOREVER {
-        motor_cmd_packet.command = MOVE;
+        motor_cmd_packet.cmd = MOVE;
         motor_cmd_packet.param1  = RIGHT_MOTOR;
         motor_cmd_packet.param2  = 75;
         xQueueSend(queue_motor_cmds, &motor_cmd_packet, portMAX_DELAY);
-        motor_cmd_packet.command = MOVE;
+        motor_cmd_packet.cmd = MOVE;
         motor_cmd_packet.param1  = LEFT_MOTOR;
         motor_cmd_packet.param2  = 75;
         xQueueSend(queue_motor_cmds, &motor_cmd_packet, portMAX_DELAY);
         vTaskDelay(3000/portTICK_PERIOD_MS);
 
-        motor_cmd_packet.command = MOTOR_BRAKE;
+        motor_cmd_packet.cmd = MOTOR_BRAKE;
         motor_cmd_packet.param1  = RIGHT_MOTOR;
         motor_cmd_packet.param2  = 0;
         xQueueSend(queue_motor_cmds, &motor_cmd_packet, portMAX_DELAY);
-        motor_cmd_packet.command = MOTOR_BRAKE;
+        motor_cmd_packet.cmd = MOTOR_BRAKE;
         motor_cmd_packet.param1  = LEFT_MOTOR;
         motor_cmd_packet.param2  = 0;
         xQueueSend(queue_motor_cmds, &motor_cmd_packet, portMAX_DELAY);
